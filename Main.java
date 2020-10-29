6e31e9eaeff8ced1a10671002d04a9f9000d4e0e
ellag7121 <ellag7121@wrdsb.ca>
2020-10-29 16:02:33 +0000

    FINISHED A8Q1!

--- Main.java
+++ Main.java
@@ -5,13 +5,16 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
 
+  JTextField nameField;
+  JLabel outputLabel;
+  JButton helloButton;
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Hello Person!");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
@@ -19,7 +22,32 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+
+    //initialize class variables
+    mainPanel = new JPanel();
+
+    nameField = new JTextField("");
+    outputLabel = new JLabel("");
+    helloButton = new JButton("Say Hello");
+
+    //add action listeners
+    helloButton.addActionListener(this);
+
+    //turn on manual layout
+    mainPanel.setLayout(null);
+
+    //set class variable size & layout
+    nameField.setBounds(250, 80, 300, 30);
+    outputLabel.setBounds(250, 170, 300, 40);
+    helloButton.setBounds(350, 260, 100, 30);
+
+    //add the class variables to the main panel
+    mainPanel.add(nameField);
+    mainPanel.add(outputLabel);
+    mainPanel.add(helloButton);
+
+    //add the main panel to the JFrame
+    frame.add(mainPanel);
 
   }
 
@@ -28,6 +56,11 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    //store the inputted name in a variable
+    String name = nameField.getText();
+
+    //make the label say hello to that name
+    outputLabel.setText("Hello " + name);
   }
 
   // Main method to start our program
